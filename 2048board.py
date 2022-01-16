1e42619983c2149663919c127bd3d08caece2828
TamKengHong <59087730+TamKengHong@users.noreply.github.com>
2022-01-16 11:15:15 +0800

    cut code to 42 lines

--- 2048board.py
+++ 2048board.py
@@ -1,9 +1,7 @@
-arr = [list(map(int, input().split())) for _ in range(4)]
-d = int(input())
+arr, d = [list(map(int, input().split())) for _ in range(4)], int(input())
 
 class Board:
-    def __init__(self, board_state):
-        self.board_state = board_state
+    def __init__(self, board_state): self.board_state = board_state
 
     def move(self, start_pos, end_pos):
         piece = self.board_state[start_pos[0]][start_pos[1]]
@@ -15,39 +13,30 @@ class Board:
             for j in range(4):
                 dict1 = {(0, -1): self.board_state[i][j], (-1, 0): self.board_state[j][i],
                          (0, 1): self.board_state[i][3 - j], (1, 0): self.board_state[3 - j][i]}
-                tile = dict1[direction]
-                if tile is not None:
-                    tile.shift(direction, self)
+                if dict1[direction] is not None: dict1[direction].shift(direction, self)
 
 class Tile:
-    def __init__(self, number):
-        self.number, self.row, self.col, self.merged = number, None, None, False
+    def __init__(self, number): self.number, self.row, self.col, self.merged = number, None, None, False
 
     def shift(self, direction, board):
         while self.row + direction[0] in range(4) and self.col + direction[1] in range(4):
             new_pos = (self.row + direction[0], self.col + direction[1])
             tile = board.board_state[new_pos[0]][new_pos[1]]
-            if tile is None:
+            if tile is None: 
                 board.move((self.row, self.col), new_pos)
             else:
                 if tile.number == self.number and tile.merged == False:
-                    self.number, self.merged = self.number * 2, True
-                    board.move((self.row, self.col), new_pos)
+                    self.number, self.merged = self.number * 2, True; board.move((self.row, self.col), new_pos)
                 break
 
-
-def board_state_converter(arr): #works
+def board_state_converter(arr):
     board_state = [[None] * 4 for i in range(4)]
     for i in range(4):
         for j in range(4):
             if arr[i][j] != 0:
-                board_state[i][j] = Tile(arr[i][j])
-                board_state[i][j].row, board_state[i][j].col = i, j
+                board_state[i][j] = Tile(arr[i][j]); board_state[i][j].row, board_state[i][j].col = i, j
     return board_state
 
-def print_board_state(board_state):
-    for i in range(4): print(*[elem.number if elem is not None else 0 for elem in board_state[i]])
-
 b, dict_d = Board(board_state_converter(arr)), {0: (0, -1), 1: (-1, 0), 2: (0, 1), 3: (1, 0)}
-b.shift_all_tiles(dict_d[d])
-print_board_state(b.board_state)
+b.shift_all_tiles(dict_d[d]);
+for i in range(4): print(*[elem.number if elem is not None else 0 for elem in b.board_state[i]])
